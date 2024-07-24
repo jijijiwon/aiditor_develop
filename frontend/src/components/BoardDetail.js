b7584e2d2786fb661c5436d6e397a0cb56e27918
Sy3058 <apo1803058@gmail.com>
2024-07-24 03:22:10 +0000

    fix: add loading code

--- frontend/src/components/BoardDetail.js
+++ frontend/src/components/BoardDetail.js
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import Loading from "./Loading";
 
 const BoardDetail = ({
   baseurl,
@@ -17,6 +18,7 @@ const BoardDetail = ({
   const [replies, setReplies] = useState([]);
   const [reply, setReply] = useState("");
   const [replyNum, setReplyNum] = useState(0);
+  const [isLoading, setIsLoading] = useState(0);
 
   async function getReply(boardnum) {
     const response = await axios.get(baseurl + "/getreply", {
@@ -64,6 +66,7 @@ const BoardDetail = ({
   }
 
   const handleReplySubmit = async () => {
+    setIsLoading(1);
     const data1 = {
       boardnum: boardnum,
       content: reply,
@@ -84,6 +87,7 @@ const BoardDetail = ({
       replynum: result.length,
     };
     await updateReplynum(data2);
+    setIsLoading(0);
   };
 
   useEffect(() => {
@@ -97,68 +101,74 @@ const BoardDetail = ({
   }, [boardnum, email]);
 
   return (
-    <div className="board-detail">
-      {email === wremail || isAdmin === 1 ? (
-        <>
-          <div className="detail-header">
-            <h1>{title}</h1>
-            <p>{wrdate}</p>
-          </div>
-          <div className="detail-content">
-            <p>{content}</p>
-          </div>
-          <div className="detail-reply">
-            <h4> 댓글 ({replyNum})</h4>
-            {replies.map((reply, i) => (
-              <div key={i} className="detail-reply-content">
-                <div className="reply-writer">
-                  <img src="../images/logo.png" alt="logo" />
-                  <span>AIditor</span>
-                </div>
-                <div className="reply-content">
-                  <h5>{reply.content}</h5>
-                  <p>{reply.wrdate}</p>
-                </div>
-              </div>
-            ))}
-          </div>
-        </>
+    <>
+      {isLoading ? (
+        <Loading />
       ) : (
-        <>
-          <div className="detail-header">
-            <h1>{title}</h1>
-            <p>{wrdate}</p>
+        <div className="board-detail">
+          {email === wremail || isAdmin === 1 ? (
+            <>
+              <div className="detail-header">
+                <h1>{title}</h1>
+                <p>{wrdate}</p>
+              </div>
+              <div className="detail-content">
+                <p>{content}</p>
+              </div>
+              <div className="detail-reply">
+                <h4> 댓글 ({replyNum})</h4>
+                {replies.map((reply, i) => (
+                  <div key={i} className="detail-reply-content">
+                    <div className="reply-writer">
+                      <img src="../images/logo.png" alt="logo" />
+                      <span>AIditor</span>
+                    </div>
+                    <div className="reply-content">
+                      <h5>{reply.content}</h5>
+                      <p>{reply.wrdate}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="detail-header">
+                <h1>{title}</h1>
+                <p>{wrdate}</p>
+              </div>
+              <div className="detail-content">
+                <p>작성자만 확인할 수 있는 게시글입니다.</p>
+              </div>
+              <div className="detail-reply">
+                <h4> 댓글 ({replyNum})</h4>
+              </div>
+            </>
+          )}
+          {isAdmin === 1 ? (
+            <>
+              <div className="detail-reply-form">
+                <textarea
+                  rows="3"
+                  name="write_reply"
+                  value={reply}
+                  onChange={(e) => setReply(e.target.value)}
+                ></textarea>
+                <input
+                  type="button"
+                  value="등록"
+                  id="reply_submit_btn"
+                  onClick={handleReplySubmit}
+                />
+              </div>
+            </>
+          ) : null}
+          <div className="detail-footer">
+            <Link to="/board">&lt; 목록으로</Link>
           </div>
-          <div className="detail-content">
-            <p>작성자만 확인할 수 있는 게시글입니다.</p>
-          </div>
-          <div className="detail-reply">
-            <h4> 댓글 ({replyNum})</h4>
-          </div>
-        </>
+        </div>
       )}
-      {isAdmin === 1 ? (
-        <>
-          <div className="detail-reply-form">
-            <textarea
-              rows="3"
-              name="write_reply"
-              value={reply}
-              onChange={(e) => setReply(e.target.value)}
-            ></textarea>
-            <input
-              type="button"
-              value="등록"
-              id="reply_submit_btn"
-              onClick={handleReplySubmit}
-            />
-          </div>
-        </>
-      ) : null}
-      <div className="detail-footer">
-        <Link to="/board">&lt; 목록으로</Link>
-      </div>
-    </div>
+    </>
   );
 };
 
